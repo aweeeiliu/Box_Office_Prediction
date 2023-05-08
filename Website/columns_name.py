72391a06237de0c8deee7386f11475b6d432688d
kuanweiliu <124176062+aweeeiliu@users.noreply.github.com>
2023-05-08 15:38:16 +0800

    上傳期末專題所需的所有元件
    Our final project @ iSpan was demonstrated by website, which generated by Python Flask. Web server is ngrok.

---
+++ Website/columns_name.py
@@ -0,0 +1,12 @@
+# match_df = df2[['genres_position0', 'genres_position1',
+#        'genres_position2','genres_freq','production_companies_position0', 'production_companies_position1','production_companies_freq','director_position0','director_freq']]
+
+genres_pos0 = 'genres_position0'
+genres_pos1 = 'genres_position1'
+genres_pos2 = 'genres_position2'
+company_pos0 = 'production_companies_position0'
+company_pos1 = 'production_companies_position1'
+director_pos0 = 'director_position0'
+genres_freq = 'genres_freq'
+production_companies_freq = 'production_companies_freq'
+director_freq = 'director_freq'
