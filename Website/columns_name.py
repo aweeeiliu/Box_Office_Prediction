a31e654bba972434a42362b2eaa1ed870fbb3ead
kuanweiliu <124176062+aweeeiliu@users.noreply.github.com>
2023-05-09 14:16:00 +0800

    新增column names說明

--- Website/columns_name.py
+++ Website/columns_name.py
@@ -1,5 +1,7 @@
-# match_df = df2[['genres_position0', 'genres_position1',
-#        'genres_position2','genres_freq','production_companies_position0', 'production_companies_position1','production_companies_freq','director_position0','director_freq']]
+# 在網站中我們放入了三個genre欄位供選擇,使用者可以決定三種都填入,或是選一種+選兩個'no option'
+# 製片商我們選擇top20,第二個製片商選項可以選others or no option; 由於top20製片商很少彼此合作,所以選擇一間top20以後,看不見別間top20很正常
+# 導演選項全數放入
+# 有關freq者,都是將使用者選擇的組合,進行頻率編碼,網頁上看不見
 
 genres_pos0 = 'genres_position0'
 genres_pos1 = 'genres_position1'
